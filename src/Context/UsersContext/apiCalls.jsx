d491ce8e8854458242dd0dd2870390224775defc
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-04-01 17:48:42 +0530

    Update apiCalls.jsx

--- src/Context/UsersContext/apiCalls.jsx
+++ src/Context/UsersContext/apiCalls.jsx
@@ -1,5 +1,5 @@
 const API_KEY = '4008ea8497eda5d3e80f32017f7d35bc';
-const url = "http://127.0.0.1:4000";
+const url = "https://the-stream-backend.vercel.app";
 import axios from "axios"
 import {getUserSuccess,getUserStart,getUserFailure
 ,	deleteUserFailure,deleteUserSuccess,deleteUserStart, createUserStart, createUserSuccess, createUserFailure, loginUserSuccess, loginUserStart} from "./UserActions";
